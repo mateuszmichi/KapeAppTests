e48267dff005f7e910c15d971e8db05610a90800
mateuszmichi <37980373+mateuszmichi@users.noreply.github.com>
2019-02-02 16:47:36 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button>Test PDF generation</button>
+        <button>Test Recharts</button>
+        <button>Test Indicators</button>
+      </div>
+    );
+  }
+}
+
+export default App;
