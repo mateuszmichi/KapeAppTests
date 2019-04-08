39927cd087cc44c0bfda883be2c6af9cfd55a6c2
mateuszmichi <37980373+mateuszmichi@users.noreply.github.com>
2019-04-08 23:05:18 +0200

    Updates

---
+++ precache-manifest.88f56fac34506762f608f39ed70e2013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e9506e7050ba7dd3e8f",
+    "url": "/KapeAppTests/static/js/runtime~main.7e9506e7.js"
+  },
+  {
+    "revision": "0d9bcde37bd4b6484931",
+    "url": "/KapeAppTests/static/js/main.0d9bcde3.chunk.js"
+  },
+  {
+    "revision": "96d01eb5b52ac3068550",
+    "url": "/KapeAppTests/static/js/1.96d01eb5.chunk.js"
+  },
+  {
+    "revision": "0d9bcde37bd4b6484931",
+    "url": "/KapeAppTests/static/css/main.291f765e.chunk.css"
+  },
+  {
+    "revision": "96d01eb5b52ac3068550",
+    "url": "/KapeAppTests/static/css/1.b6bb743a.chunk.css"
+  },
+  {
+    "revision": "0deda6f9b6a9e785820ed7999e16ee33",
+    "url": "/KapeAppTests/index.html"
+  }
+];
